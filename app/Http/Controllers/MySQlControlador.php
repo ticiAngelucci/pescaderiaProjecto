b99c0c4b8c552a1773167bc41f1c2599af34c99e
Tici Angelucci <tittiangelucci08@gmail.com>
2023-04-28 14:51:11 -0300

    error bd no resuelvo aun pero nueva rama

--- app/Http/Controllers/MySQlControlador.php
+++ app/Http/Controllers/MySQlControlador.php
@@ -11,4 +11,12 @@ class MySQlControlador extends Controller
     public function obtenerProductos(){
         return productos::all();
     }
+    public function crearProductos(Request $request){
+        $productoNuevo = new productos;
+        $productoNuevo->nombre = $request->producto;
+        $productoNuevo->id_categoria = $request->id_categoria;
+        $productoNuevo->precio_por_gramo = $request->precio;
+        $productoNuevo->save();
+        return back()->with('mensaje','Producto agregado');
+    }
 }
--- app/Models/productos.php
+++ app/Models/productos.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class productos extends Model
 {
+    protected $table = 'productos';
+    protected $primaryKey = 'id_producto';
     use HasFactory;
 }
