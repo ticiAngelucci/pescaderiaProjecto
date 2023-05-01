161c34fe2c44f12b1dcdb053583ef9126af339fb
Tici Angelucci <tittiangelucci08@gmail.com>
2023-05-01 13:48:28 -0300

    header and footer sections

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function () {
+    return view('login');
+});
 Route::get('/', function () {
-    return view('welcome');
+    return view('catalogo');
 });
 
 Route::get('/productos', "App\Http\Controllers\MySQlControlador@obtenerProductos");
