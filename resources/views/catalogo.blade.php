7c503211c1bbd2313394779e0c6e9003a3bf51f1
Tici Angelucci <tittiangelucci08@gmail.com>
2023-05-02 01:15:36 -0300

    haciendo vista de catalogo 1/2

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -1,23 +1,28 @@
 @include('header')
 @include('navbar')
-
-
-            <div>
-                        <div>
-                            Catalogo de Productos
-                        </div>
-                            @foreach($productos as $producto)
-                            <div class="col">
-                                <img src="{{$producto->img}}"/>
-                                <h4>{{$producto->nombre}}</h4>
-                                <h4>Precio:{{$producto->precio_por_gramo}}</h4>
+        <div>
+            Catalogo de Productos
+        </div>
+        <div class="container">
+            <div class="row">
+                    @foreach($productos as $producto)
+                    <div class="card shadow col-3 ">
+                        <img src="https://i.ibb.co/XCNWQHD/principal.jpg">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$producto->nombre}}</h5>
+                            <p class="card-text">$ {{$producto->precio_por_gramo}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="{{route('detalle',$producto->id_producto)}}" class="btn btn-primary">Detalles</a>
+                                </div>
+                                <a href="#" class="btn btn-success">Agregar</a>
                             </div>
-                                <a  href="{{route('detalle',$producto->id_producto)}}">{{__('Mas detalle')}}</a>
-                                <button>Agregar carrito</button>
-                            @endforeach
-                    
-                    <center class="mt-5">
-                        {{ $productos->links('paginator') }}
-                    </center>
+                        </div>
+                    </div>
+                    @endforeach
             </div>
+                <center class="mt-5">
+                        {{ $productos->links('paginator') }}
+                </center>
+        </div>
 @include('footer')
