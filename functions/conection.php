3aa3b1472dc851e083f2032259cb1b58358ffb25
ticiAngelucci <tittiangelucci08@gmail.com>
2023-06-05 19:56:04 -0300

    agregar productos add falta volver a inicio

--- functions/conection.php
+++ functions/conection.php
@@ -9,5 +9,5 @@ $conexion = new mysqli($server,$user,$pass,$db);
 if($conexion->connect_errno){
     die("La conexion ha fallado".$conexion->connect_errno);
 }else{
-    echo "Conexion exitosa";
+   
 }
