b57b1e6f7a31b5001d183de31cf0986a977ef6d4
Luca <lucasantacroee@gmail.com>
2023-06-11 21:16:25 -0300

    CarritoDeCompras9deLanoche

--- inicio.php
+++ inicio.php
@@ -1,33 +1,32 @@
-<?php 
- include('components/header.php');
- include('components/navbar.php'); 
- include('functions/conection.php');
- include('functions/config.php');
- include_once('functions/cart.php');
+<?php
+include('components/header.php');
+include('components/navbar.php');
+include('functions/conection.php');
+include('functions/config.php');
+include_once('functions/cart.php');
 
-
-$consulta="SELECT id_producto,nombre,precio_por_gramo FROM productos";
-$resultados=mysqli_query($conexion,$consulta);
+$consulta = "SELECT id_producto, nombre, precio_por_gramo, cantidad_disponible FROM productos";
+$resultados = mysqli_query($conexion, $consulta);
 $productosConImg = [
-'langostino' =>
-'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/215/401/products/lango-pelado1-5ff98af31ed78eae3b16496944392552-1024-1024.jpeg',
-'camaron' =>
-'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGlb4HIOzaEyeUKma9JthR-OIy0-eB6hCM5JjWv-O9bU5QZkNimdIuUI3iWkuBwQDZJS4&usqp=CAU',
-'Doritos' => 'https://http2.mlstatic.com/D_NQ_NP_693032-MLA46951185556_082021-O.jpg',
-'pringles' =>
-'https://jumboargentina.vtexassets.com/arquivos/ids/766445/Papas-Pringles-Original-X124gs-1-944089.jpg?v=638104306094430000',
-'merluza' => 'https://www.consumer.es/app/uploads/fly-images/240582/merluza-de-pincho-1200x550-cc.jpg',
-'salmon' =>
-'https://ichef.bbci.co.uk/news/640/amz/worldservice/live/assets/images/2014/12/10/141210153740_salmon_promos__624x351_thinkstock.jpg',
-'queeeeedate' =>
-'https://www.billboard.com/wp-content/uploads/2023/01/Quevedo-2023-billboard-espanol-1548.jpg?w=942&h=623&crop=1&resize=942%2C623',
-'sprite' =>
-'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/188/828/products/images-111-ae3485bd0f9a65d0be16529739175163-640-0.jpg',
-'default' =>
-'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIZcQj0m_KyB8nZOnvGmDjb50YpXC3b3OmiobpDM0kejAScsWT_bpl_QGeUTUIUyWCT0s&usqp=CAU',
-];
-
+    'langostino' =>
+    'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/215/401/products/lango-pelado1-5ff98af31ed78eae3b16496944392552-1024-1024.jpeg',
+    'camaron' =>
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGlb4HIOzaEyeUKma9JthR-OIy0-eB6hCM5JjWv-O9bU5QZkNimdIuUI3iWkuBwQDZJS4&usqp=CAU',
+    'Doritos' => 'https://http2.mlstatic.com/D_NQ_NP_693032-MLA46951185556_082021-O.jpg',
+    'pringles' =>
+    'https://jumboargentina.vtexassets.com/arquivos/ids/766445/Papas-Pringles-Original-X124gs-1-944089.jpg?v=638104306094430000',
+    'merluza' => 'https://www.consumer.es/app/uploads/fly-images/240582/merluza-de-pincho-1200x550-cc.jpg',
+    'salmon' =>
+    'https://ichef.bbci.co.uk/news/640/amz/worldservice/live/assets/images/2014/12/10/141210153740_salmon_promos__624x351_thinkstock.jpg',
+    'queeeeedate' =>
+    'https://www.billboard.com/wp-content/uploads/2023/01/Quevedo-2023-billboard-espanol-1548.jpg?w=942&h=623&crop=1&resize=942%2C623',
+    'sprite' =>
+    'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/188/828/products/images-111-ae3485bd0f9a65d0be16529739175163-640-0.jpg',
+    'default' =>
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIZcQj0m_KyB8nZOnvGmDjb50YpXC3b3OmiobpDM0kejAScsWT_bpl_QGeUTUIUyWCT0s&usqp=CAU',
+    ];
 ?>
+
 <section class="section-products">
     <div class="container">
         <div class="row justify-content-center text-center">
@@ -39,41 +38,45 @@ $productosConImg = [
             </div>
         </div>
         <div class="row">
-            <?php
-                foreach($resultados as $producto) { ?>
+            <?php foreach ($resultados as $producto) { ?>
             <div class="col-md-6 col-lg-4 col-xl-3">
                 <div id="product-1" class="single-product">
-                    <div class="part-1" style="background: url(<?php if(isset($productosConImg[$producto['nombre']])) { echo $productosConImg[$producto['nombre']]; } else{ echo $productosConImg['default'];}?>)
-                         no-repeat center !important;">
-                    </div>
+                    <div class="part-1" style="background: url(<?php if (isset($productosConImg[$producto['nombre']])) {
+    echo $productosConImg[$producto['nombre']];
+} else {
+    echo $productosConImg['default'];
+} ?>) no-repeat center !important;"></div>
                     <div class="part-2">
-                        <h3 class="product-title"><?php echo $producto['nombre'];?></h3>
-                        <h4 class="product-price">$ <?php echo number_format($producto['precio_por_gramo'],2,'.',',');?>
-                            </h3>
+                        <h3 class="product-title"><?php echo $producto['nombre']; ?></h3>
+                        <h4 class="product-price">$
+                            <?php echo number_format($producto['precio_por_gramo'], 2, '.', ','); ?></h3>
                         </h4>
                     </div>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <a href="detalleProducto.php?id_producto=<?php echo $producto['id_producto'];?>&token=<?php echo hash_hmac('sha1',$producto['id_producto'],KEY_TOKEN); ?>"
+                            <a href="detalleProducto.php?id_producto=<?php echo $producto['id_producto']; ?>&token=<?php echo hash_hmac('sha1', $producto['id_producto'], KEY_TOKEN); ?>"
                                 class="btn btn-primary">Detalles</a>
                         </div>
                         <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                             <input type="hidden" name="id_producto" value="<?php echo $producto['id_producto']; ?>">
+                            <input type="hidden" name="nombre" value="<?php echo $producto['nombre']; ?>">
+                            <input type="hidden" name="precio_por_gramo"
+                                value="<?php echo $producto['precio_por_gramo']; ?>">
+                            <input type="hidden" name="cantidad_disponible"
+                                value="<?php echo $producto['cantidad_disponible']; ?>">
                             <button class="btn btn-primary" name="accionBoton" value="Agregar"
                                 type="submit">Agregar</button>
                         </form>
 
-
                     </div>
                 </div>
-                <?php 
-                }
-            ?>
             </div>
+            <?php } ?>
         </div>
         <center class="mt-5">
             <!--  {{ $productos->links('paginator') }} -->
         </center>
+    </div>
 </section>
 
 <?php include('components/footer.php'); ?>
