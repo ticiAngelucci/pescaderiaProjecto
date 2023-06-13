3968007419c1d6ba5ac3eb05284c6e8570fe39e2
Luca <lucasantacroee@gmail.com>
2023-06-13 20:01:32 -0300

    PerdonTiciAhiAndaElcarrito

--- functions/cart.php
+++ functions/cart.php
@@ -1,31 +1,60 @@
-<?php session_start();
+<?php
+include("functions/conection.php");
 
-if(isset($_SESSION['carrito'])){
-    $carrito_mio=$_SESSION['carrito'];
-    if(isset($_POST['nombre'])){
-        //precio_por_gramo
-        //cantidad_disponible
-        $nombre=$_POST['nombre'];
-        $precio=$_POST['precio_por_gramo'];
-        $cantidad=$_POST['cantidad_disponible'];
-        $num=0;
-        $carrito_mio[]=array("nombre"=>$nombre,"precio"=>$precio,"cantidad"=>$cantidad);
+if (!isset($_SESSION['carrito'])) {
+    $_SESSION['carrito'] = array(); // Inicializar como un arreglo vacío
+}
 
-    }else{
-        $nombre=$_POST['nombre'];
-        $precio=$_POST['precio_por_gramo'];
-        $cantidad=$_POST['cantidad_disponible'];
-        $carrito_mio[]=array("nombre"=>$nombre,"precio"=>$precio,"cantidad"=>$cantidad);
+
+function obtenerProductoPorId($idProducto) {
+    global $conexion;
+    $idProducto = mysqli_real_escape_string($conexion, $idProducto);
+    $sql = "SELECT * FROM productos WHERE id_producto = ?";
+    $stmt = $conexion->prepare($sql);
+    $stmt->bind_param("i", $idProducto);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+
+    if ($resultado && $resultado->num_rows > 0) {
+        $producto = $resultado->fetch_assoc();
+        return $producto;
     }
 
-
-    $_SESSION['carrito']=$carrito_mio;
-
+    return null;
 }
-header("Location" .$SERVER['HTTP_REFERER']);
+
+function agregarProductoAlCarrito($idProducto, $cantidad) {
+    global $conexion;
+    $producto = obtenerProductoPorId($idProducto);
+
+    if ($producto) {
+        $encontrado = false;
+        foreach ($_SESSION['carrito'] as $indice => $item) {
+            if ($item['id_producto'] == $idProducto) {
+                $_SESSION['carrito'][$indice]['cantidad_disponible'] += $cantidad;
+                $encontrado = true;
+                break;
+            }
+        }
+
+        if (!$encontrado) {
+            $producto['cantidad_disponible'] = $cantidad;
+            $_SESSION['carrito'][] = $producto;
+        }
+
+        echo '<script>alert("El producto se ha agregado al carrito");</script>';
+    } else {
+        echo '<script>alert("El producto seleccionado no existe");</script>';
+    }
+}
 
 
 
-
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['accionBoton']) && $_POST['accionBoton'] === 'Agregar') {
+        $idProducto = mysqli_real_escape_string($conexion, $_POST['id_producto']);
+        $cantidad = intval($_POST['cantidad_disponible']);
+        agregarProductoAlCarrito($idProducto, $cantidad);
+    }
+}
 ?>
--- inicio.php
+++ inicio.php
@@ -1,11 +1,11 @@
 <?php 
- include('components/header.php');
- include('components/navbar.php'); 
- include('functions/conection.php');
- include('functions/config.php');
- $consulta="SELECT id_producto,nombre,precio_por_gramo FROM productos";            
- $resultados=mysqli_query($conexion,$consulta); 
- $productosConImg = [
+session_start();
+include('components/header.php');
+include('components/navbar.php'); 
+include('functions/conection.php');
+include('functions/config.php');
+include_once('functions/cart.php');
+$productosConImg = [
     'langostino' => 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/215/401/products/lango-pelado1-5ff98af31ed78eae3b16496944392552-1024-1024.jpeg',
     'camaron' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGlb4HIOzaEyeUKma9JthR-OIy0-eB6hCM5JjWv-O9bU5QZkNimdIuUI3iWkuBwQDZJS4&usqp=CAU',
     'Doritos' => 'https://http2.mlstatic.com/D_NQ_NP_693032-MLA46951185556_082021-O.jpg',
@@ -16,18 +16,22 @@
     'sprite' => 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/188/828/products/images-111-ae3485bd0f9a65d0be16529739175163-640-0.jpg',
     'default' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIZcQj0m_KyB8nZOnvGmDjb50YpXC3b3OmiobpDM0kejAScsWT_bpl_QGeUTUIUyWCT0s&usqp=CAU',
  ];
-$activo=0;
-$filter=0;
+$consulta = "SELECT id_producto, nombre, precio_por_gramo, cantidad_disponible FROM productos";            
+$resultados = mysqli_query($conexion, $consulta); 
+$activo = 0;
+$filter = 0;
+
 if(isset($_POST['btnbuscar'])){
-    $activo=1;
+    $activo = 1;
     $busqueda = $_POST['busqueda'];
-    $consulta="SELECT * FROM productos WHERE nombre LIKE '%$busqueda%' ";            
-    $queryBusqueda=mysqli_query($conexion,$consulta); 
-};
+    $consulta = "SELECT * FROM productos WHERE nombre LIKE '%$busqueda%'";            
+    $queryBusqueda = mysqli_query($conexion, $consulta); 
+}
+
 if(isset($_POST['btnfiltrar'])){
     $ordenamiento = $_POST['ordenamiento'];
-    $activo=1;
-    $filter=1;
+    $activo = 1;
+    $filter = 1;
     if ($ordenamiento == 'nombre_asc') {
         $resultadosFiltrar = mysqli_query($conexion, "SELECT * FROM productos ORDER BY nombre ASC");
     } elseif ($ordenamiento == 'nombre_desc') {
@@ -39,6 +43,7 @@ if(isset($_POST['btnfiltrar'])){
     }
 }
 ?>
+
 <section class="section-products">
     <div class="container">
         <div class="row justify-content-center text-center">
@@ -49,9 +54,11 @@ if(isset($_POST['btnfiltrar'])){
                 </div>
             </div>
         </div>
-        <form method="POST"><input type="text" name="busqueda"><input type="submit" value="buscar" name="btnbuscar" />
+        <form method="POST">
+            <input type="text" name="busqueda">
+            <input type="submit" value="buscar" name="btnbuscar" />
         </form>
-        <form method="post"  style="margin-bottom:40px;">
+        <form method="post" style="margin-bottom:40px;">
             <div class="form-group">
                 <label for="ordenamiento">Ordenar por:</label>
                 <select class="form-control" id="ordenamiento" name="ordenamiento">
@@ -66,83 +73,101 @@ if(isset($_POST['btnfiltrar'])){
     </div>
     <div class="row" style="max-width: 1100px !important;margin: auto !important;">
         <?php 
-        if($activo==0){
+        if($activo == 0){
+            foreach($resultados as $producto) {
         ?>
-        <?php
-                foreach($resultados as $producto) { ?>
         <div class="col-md-6 col-lg-4 col-xl-3">
             <div id="product-1" class="single-product">
-                <div class="part-1" style="background: url(<?php if(isset($productosConImg[$producto['nombre']])) { echo $productosConImg[$producto['nombre']]; } else{ echo $productosConImg['default'];}?>)
-                         no-repeat center !important;">
+                <div class="part-1"
+                    style="background: url(<?php if(isset($productosConImg[$producto['nombre']])) { echo $productosConImg[$producto['nombre']]; } else{ echo $productosConImg['default'];}?>) no-repeat center !important;">
                 </div>
                 <div class="part-2">
                     <h3 class="product-title"><?php echo $producto['nombre'];?></h3>
-                    <h4 class="product-price">$
-                        <?php echo number_format($producto['precio_por_gramo'],2,'.',',');?>
-                        </h3>
-                    </h4>
+                    <h4 class="product-price">$<?php echo number_format($producto['precio_por_gramo'],2,'.',',');?></h4>
                 </div>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <a href="detalleProducto.php?id_producto=<?php echo $producto['id_producto'];?>&token=<?php echo hash_hmac('sha1',$producto['id_producto'],KEY_TOKEN); ?>"
                             class="btn btn-primary">Detalles</a>
                     </div>
-                    <a href="#" class="btn btn-success">Agregar</a>
+                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <input type="hidden" name="id_producto" value="<?php echo $producto['id_producto']; ?>">
+                        <input type="hidden" name="nombre" value="<?php echo $producto['nombre']; ?>">
+                        <input type="hidden" name="precio_por_gramo"
+                            value="<?php echo $producto['precio_por_gramo']; ?>">
+                        <input type="hidden" name="cantidad_disponible"
+                            value="<?php echo isset($producto['cantidad_disponible']) ? $producto['cantidad_disponible'] : ''; ?>">
+                        <button class="btn btn-primary" name="accionBoton" value="Agregar"
+                            type="submit">Agregar</button>
+                    </form>
+
                 </div>
             </div>
         </div>
         <?php 
-                }
-        ?>
-        <?php
-        }else{
-            if($filter==0 && $activo==1){
-            foreach($queryBusqueda as $busquedaProducto) { 
+            }
+        } else {
+            if($filter == 0 && $activo == 1){
+                foreach($queryBusqueda as $busquedaProducto) {
         ?>
         <div class="col-md-4 col-xl-3" style="margin-top:50px;">
             <div id="product-1" class="single-product">
-                <div class="part-1" style="background: url(<?php if(isset($productosConImg[$busquedaProducto['nombre']])) { echo $productosConImg[$busquedaProducto['nombre']]; } else{ echo $productosConImg['default'];}?>)
-                         no-repeat center !important;">
+                <div class="part-1"
+                    style="background: url(<?php if(isset($productosConImg[$busquedaProducto['nombre']])) { echo $productosConImg[$busquedaProducto['nombre']]; } else{ echo $productosConImg['default'];}?>) no-repeat center !important;">
                 </div>
                 <div class="part-2">
                     <h3 class="product-title"><?php echo $busquedaProducto['nombre'];?></h3>
-                    <h4 class="product-price">$
-                        <?php echo number_format($busquedaProducto['precio_por_gramo'],2,'.',',');?>
-                        </h3>
-                    </h4>
+                    <h4 class="product-price">
+                        $<?php echo number_format($busquedaProducto['precio_por_gramo'],2,'.',',');?></h4>
                 </div>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <a href="detalleProducto.php?id_producto=<?php echo $busquedaProducto['id_producto'];?>&token=<?php echo hash_hmac('sha1',$busquedaProducto['id_producto'],KEY_TOKEN); ?>"
                             class="btn btn-primary">Detalles</a>
                     </div>
-                    <a href="#" class="btn btn-success">Agregar</a>
+                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <input type="hidden" name="id_producto" value="<?php echo $busquedaProducto['id_producto']; ?>">
+                        <input type="hidden" name="nombre" value="<?php echo $busquedaProducto['nombre']; ?>">
+                        <input type="hidden" name="precio_por_gramo"
+                            value="<?php echo $busquedaProducto['precio_por_gramo']; ?>">
+                        <input type="hidden" name="cantidad_disponible"
+                            value="<?php echo $busquedaProducto['cantidad_disponible']; ?>">
+                        <button class="btn btn-primary" name="accionBoton" value="Agregar"
+                            type="submit">Agregar</button>
+                    </form>
                 </div>
             </div>
         </div>
-        <?php
-            }  
-        }else{
-            foreach($resultadosFiltrar as $resultadoFiltrar) { 
-    ?>
+        <?php 
+                }
+        } else {
+            foreach($resultadosFiltrar as $resultadoFiltrar) {
+        ?>
         <div class="col-md-6 col-lg-4 col-xl-3">
             <div id="product-1" class="single-product">
-                <div class="part-1" style="background: url(<?php if(isset($productosConImg[$resultadoFiltrar['nombre']])) { echo $productosConImg[$resultadoFiltrar['nombre']]; } else{ echo $productosConImg['default'];}?>)
-                         no-repeat center !important;">
+                <div class="part-1"
+                    style="background: url(<?php if(isset($productosConImg[$resultadoFiltrar['nombre']])) { echo $productosConImg[$resultadoFiltrar['nombre']]; } else{ echo $productosConImg['default'];}?>) no-repeat center !important;">
                 </div>
                 <div class="part-2">
                     <h3 class="product-title"><?php echo $resultadoFiltrar['nombre'];?></h3>
-                    <h4 class="product-price">$
-                        <?php echo number_format($resultadoFiltrar['precio_por_gramo'],2,'.',',');?>
-                        </h3>
-                    </h4>
+                    <h4 class="product-price">
+                        $<?php echo number_format($resultadoFiltrar['precio_por_gramo'],2,'.',',');?></h4>
                 </div>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <a href="detalleProducto.php?id_producto=<?php echo $resultadoFiltrar['id_producto'];?>&token=<?php echo hash_hmac('sha1',$resultadoFiltrar['id_producto'],KEY_TOKEN); ?>"
                             class="btn btn-primary">Detalles</a>
                     </div>
-                    <a href="#" class="btn btn-success">Agregar</a>
+                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <input type="hidden" name="id_producto" value="<?php echo $resultadoFiltrar['id_producto']; ?>">
+                        <input type="hidden" name="nombre" value="<?php echo $resultadoFiltrar['nombre']; ?>">
+                        <input type="hidden" name="precio_por_gramo"
+                            value="<?php echo $resultadoFiltrar['precio_por_gramo']; ?>">
+                        <input type="hidden" name="cantidad_disponible"
+                            value="<?php echo $resultadoFiltrar['cantidad_disponible']; ?>">
+                        <button class="btn btn-primary" name="accionBoton" value="Agregar"
+                            type="submit">Agregar</button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -151,7 +176,6 @@ if(isset($_POST['btnfiltrar'])){
         }
     }
     ?>
-    </div>
     </div>
     <center class="mt-5">
         <!--  {{ $productos->links('paginator') }} -->
