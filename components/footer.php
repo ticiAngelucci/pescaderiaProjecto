6ecf2b802a1680ecb7d3098dd5fe75fd9c839f97
ticiAngelucci <tittiangelucci08@gmail.com>
2023-06-25 16:39:48 -0300

    filtrar en historial

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,4 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- historial.php
+++ historial.php
@@ -9,47 +9,80 @@ $id_usuario = $_SESSION['id_usuario'];
 echo $id_usuario;
 include('components/navbar.php');
 $consulta = "SELECT * FROM pedidos where id_cliente='$id_usuario'";
+if (isset($_POST['btnfiltrar'])) {
+    $ordenarPor = $_POST['ordenar_por'];
+    $orden = $_POST['orden'];
+
+    $consulta .= " ORDER BY $ordenarPor $orden";
+}
+
 $resultados = mysqli_query($conexion, $consulta);
+
+if (!$resultados) {
+    echo "Error en la consulta: " . mysqli_error($conexion);
+} else {
+    if (mysqli_num_rows($resultados) === 0) {
+        echo "No se encontraron resultados.";
+    } else {
+        // Procesar los resultados
+        // ...
+    }
+}
 ?>
-<div class="row justify-content-center text-center">
-    <div class="col-md-8 col-lg-6">
-        <div class="header" style="color:white;margin-top:30px;">
-            <h2>Historial de Pedidos</h2>
-            <form method="POST"><input type="text" name="busqueda"><input type="submit" value="buscar"
-                    name="btnbuscar" />
-            </form>
-            <form method="post" style="margin-bottom:40px;">
-                <div class="form-group">
-                    <label for="ordenamiento">Ordenar por:</label>
-                    <select class="form-control" id="ordenamiento" name="ordenamiento">
-                        <option value="filtrar_empl">Filtrar solo empleados</option>
-                        <option value="filtrar_cli">Filtrar solo clientes</option>
-                        <option value="todo">Todo</option>
-                    </select>
-                </div>
-                <button type="submit" name="btnfiltrar" class="btn btn-primary">Ordenar</button>
-            </form>
-        </div>
-    </div>
-</div>
-<div class="containerPedidosHistorial" style="width: 100%;max-width: 900px;margin: auto;">
-    <?php foreach($resultados as $pedido) {  ?>
-    <div class="card" style="margin-bottom: 20px;">
-        <div class="card-header">
-            Pedido <?php echo $pedido['id_pedido']?>
-        </div>
-        <div class="card-body" style="display: flex;justify-content: space-around;align-items: center;">
-            <img class="card-img-top" src="assets/pedido.png" alt="Card image cap"
-                style="width: 100%;max-width: 250px;">
-            <div>
-                <h5 class="card-title">Tu compra se realizo <?php echo $pedido['hora_fecha_now']?></h5>
-                <p class="card-text" style="text-align: left;">Su compra se retira <?php echo $pedido['fecha_entrega_pedido']; echo "&nbsp;"; echo $pedido['hora_entrega_pedido']; ?></p>
-                <p class="card-text" style="text-align: left;">El total a pagar es $ <?php echo $pedido['total_pedido']; ?></p>
-                <a href="descripcionPedido.php" style="max-width: 150px;width: 100%;" class="btn btn-primary">Ver
-                    más</a>
+<div class="container" style="max-width: 1436px;">
+    <div class="row justify-content-center text-center">
+        <div class="col-md-8 col-lg-6">
+            <div class="header" style="color:white;margin-top:30px;">
+                <h2>Historial de Pedidos</h2>
+                <form method="POST"
+                    style="justify-content: center;align-items: flex-end;flex-wrap: nowrap;flex-direction: row;"
+                    class="form-inline mt-2">
+                    <div class="form-group mr-sm-2">
+                        <label for="ordenar_por">Ordenar por:</label>
+                        <select name="ordenar_por" id="ordenar_por" class="form-control">
+                            <option value="fecha_entrega_pedido">Fecha de entrega</option>
+                            <option value="hora_fecha_now">Fecha de compra</option>
+                            <option value="total_pedido">Total a pagar</option>
+                        </select>
+                    </div>
+                    <div class="form-group mr-sm-2">
+                        <label for="orden">Orden:</label>
+                        <select name="orden" id="orden" class="form-control">
+                            <option value="asc">Ascendente</option>
+                            <option value="desc">Descendente</option>
+                        </select>
+                    </div>
+                    <button type="submit" name="btnfiltrar" class="btn btn-primary">Filtrar</button>
+                </form>
+
             </div>
         </div>
     </div>
-    <?php } ?>
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-6">
+            <?php foreach($resultados as $pedido) {  ?>
+            <div class="card mb-3">
+                <div class="card-header">
+                    Pedido <?php echo $pedido['id_pedido']?>
+                </div>
+                <div class="card-body row">
+                    <div class="col-md-4">
+                        <img class="card-img-top" src="assets/pedido.png" alt="Card image cap"
+                            style="width: 100%;max-width: 250px;">
+                    </div>
+                    <div class="col-md-8">
+                        <h5 class="card-title">Tu compra se realizó <?php echo $pedido['hora_fecha_now']?></h5>
+                        <p class="card-text" style="text-align:left;">Su compra se retira
+                            <?php echo $pedido['fecha_entrega_pedido']; echo "&nbsp;"; echo $pedido['hora_entrega_pedido']; ?>
+                        </p>
+                        <p class="card-text" style="text-align:left;">El total a pagar es $
+                            <?php echo $pedido['total_pedido']; ?></p>
+                        <a href="descripcionPedido.php" class="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
 </div>
- <?php include('components/footer.php');?>
+<?php include('components/footer.php');?>
