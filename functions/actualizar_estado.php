eaceafe2aa5cf5e2142c12f1b3a2b4b47aa43c1a
ticiAngelucci <tittiangelucci08@gmail.com>
2023-07-05 16:49:48 -0300

    editar habitaciones

--- functions/actualizar_estado.php
+++ functions/actualizar_estado.php
@@ -1,9 +1,16 @@
 <?php
 include('conection.php');
+
 $idEstadoSiguiente = $_GET['idEstadoSiguiente'];
 $idPedido = $_GET['idPedido'];
-$consultaEstadoPedido = "INSERT INTO estados_pedidos (id_estado, id_pedido, hora_fecha_now) VALUES ('$idEstadoSiguiente','$idPedido', NOW())";
-mysqli_query($conexion, $consultaEstadoPedido);
-header("Location:../estadoPedidos.php");
+echo "idEstadoSiguiente: " . $idEstadoSiguiente . "<br>";
+echo "idPedido: " . $idPedido . "<br>";
+// Preparar la consulta utilizando una sentencia preparada
+$consultaEstadoPedido = "INSERT INTO estados_pedidos (id_estado, id_pedido, hora_fecha_now) VALUES (?, ?, NOW())";
+$statement = mysqli_prepare($conexion, $consultaEstadoPedido);
+mysqli_stmt_bind_param($statement, "ii", $idEstadoSiguiente, $idPedido);
+mysqli_stmt_execute($statement);
+
+/* header("Location: ../estadoPedidos.php"); */
 exit();
 ?>
